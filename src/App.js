dcd580a1848441da521b89d53a595d1a36bd46a9
Truong <71827048+truongnhatbui@users.noreply.github.com>
2021-07-15 10:21:33 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,41 +14,41 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>FinTech (Financial Technology)</h1>
       <h2>
-        Deployed with{' '}
+      Managed with {' '}
         <a
-          href="https://vercel.com/docs"
+          href="https://dev.azure.com/truongbn/FinTech"
           target="_blank"
           rel="noreferrer noopener"
         >
-          Vercel
+          Azure DevOps
         </a>
         !
       </h2>
       <p>
         <a
-          href="https://github.com/vercel/vercel/tree/main/examples/create-react-app"
+          href="https://dev.azure.com/truongbn/FinTech/_wiki/wikis/FinTech.wiki/3/FinTech"
           target="_blank"
           rel="noreferrer noopener"
         >
           This project
         </a>{' '}
         was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
+        <a href="https://www.truongbui.com/posts/react">
+          React
         </a>{' '}
         and contains three directories, <code>/public</code> for static assets,{' '}
         <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
+        which contains a serverless <a href="https://www.truongbui.com/posts/serverless">Go</a>{' '}
+        function. The application is packaged and deployed via <a href="https://www.truongbui.com/posts/cloud-native-application-architecture">Cloud Native Application Architecture</a>. See{' '}
         <a href="/api/date">
           <code>api/date</code> for the Date API with Go
         </a>
         .
       </p>
       <br />
-      <h2>The date according to Go is:</h2>
+      <h2>Current date:</h2>
       <p>{date ? date : 'Loading date...'}</p>
     </main>
   );
